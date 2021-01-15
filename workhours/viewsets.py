a77b7b1dbe7563aa3526624dc82836f68a819cd5
marpiotoun <plus470@gmail.com>
2021-01-16 01:59:24 +0900

    Create api/v1/workhours/team_averate_time Action

--- workhours/viewsets.py
+++ workhours/viewsets.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
-from django.db.models import Q
+from django.utils import timezone
 from rest_framework import viewsets
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .serializers import WorkHourSerializer
@@ -54,8 +55,34 @@ class WorkHourViewset(viewsets.ModelViewSet):
             serializer = self.get_serializer(workhours, many=True)
             return Response(status=status.HTTP_200_OK, data=serializer.data)
         else:
-            # user이 target_user가 아니고, target_user의 position이 user보다 높거나 같은 팀이 아니면 403
+            # 여기로 넘어온 경우는 user이 target_user가 아니고, target_user의 position이 user보다 높거나 같은 팀이 아닌 것이다
+            # 이 경우 403을 Response 로 보낸다
             return Response(status=status.HTTP_403_FORBIDDEN)
+
+    @action(detail=False, methods=["get"], permission_classes=[IsAuthenticated])
+    def team_average_time(self, request):
+        """
+        특정 month 의 사용자(user)가 소속된 팀의 평균 근무 시간을 초(seconds)로 제공한다.
+        get으로 month가 제공되지 않으면 현재 월을 기본으로 한다.
+        """
+        user = request.user
+        team = user.team
+        month = request.GET.get("month")
+        now = timezone.now()
+        if month is None:
+            month = now.month
+
+        # total은 해당 월에 모든 팀원이 근무한 근무시간의 총합을 초(seconds)로 나타낸 것이다.
+        # 만약 지난 달이면, total을 30으로 나누고, 아직 해당 월이 다 끝나지 않았으면, 지금이 몇 일인지(day) 를 가져와서 나눈다.
+        total = 0
+        for team_member in team.users.get_queryset():
+            for workhour in team_member.workhours.get_queryset():
+                if workhour.start.month == month:
+                    total += workhour.total if workhour.total is not False else 0
+        avg_per_date = int(total / now.day) if month == now.month else int(total / 30)
+        avg_per_person = int(total / team.users.get_queryset().__len__())
+        data = {"avg_per_date": avg_per_date, "avg_per_person": avg_per_person}
+        return Response(status=status.HTTP_200_OK, data=data)
 
     def get_permissions(self):
         """
