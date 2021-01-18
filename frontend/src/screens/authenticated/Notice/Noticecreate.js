c2e7ce6ef4853c27a5bca51811c8a2746beb5e2d
Cho-Eunhak <02069@naver.com>
2021-01-18 19:13:00 +0900

    공지사항 ui 완성 pagination 해야함

--- frontend/src/screens/authenticated/Notice/Noticecreate.js
+++ frontend/src/screens/authenticated/Notice/Noticecreate.js
@@ -3,9 +3,12 @@ import React,{useState} from 'react';
 import Title from '../Title';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import NoticeLayout from './NoticeLayout';
+import Noticelist from './Noticelist';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import {Link} from 'react-router-dom'
 const Noticecreate = ({match}) => {
     const { team } = match.params;
     const [desc, setDesc] = useState('');
@@ -31,8 +34,22 @@ const Noticecreate = ({match}) => {
                 <Grid item xs={12}>
                 <Paper className={classes.paper}>
                 <Title>{team} 공지사항</Title>
-                <NoticeLayout></NoticeLayout>
+                <TextField
+                    id="outlined-full-width"
+                    label="제목"
+                    placeholder="제목을 입력하세요"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="outlined"
+                    />
                 <Editor value={desc} onChange={onEditorChange} />
+
+                <Button component={Link} to={'/Notice'} variant="contained" color="primary" onClick={() => alert("저장되었습니다!")}>
+                저장하기
+                </Button>
                 </Paper>
             </Grid>
             </Grid>
