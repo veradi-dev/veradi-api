9352942eb1dda8e2252a04c930a5321f7b84a752
Cho-Eunhak <02069@naver.com>
2021-01-29 17:40:00 +0900

    [0129 은학] 회의실 예약 취소하기 ui 초안

--- frontend/src/screens/authenticated/Mainnav.js
+++ frontend/src/screens/authenticated/Mainnav.js
@@ -5,6 +5,7 @@ import { Dashboard, NoticeLayout, Project, Room, Team, Workhour } from './';
 import Noticecreate from './Notice/Noticecreate';
 import Notice from './Notice/Notice';
 import Noticedetail from './Notice/Noticedetail';
+import CancelRoom from './Room/CancelRoom';
 const MainNav = () => {
 	return (
 		<>
@@ -16,7 +17,7 @@ const MainNav = () => {
                   <Route exact path="/workhour" component={ Workhour }/>
                   <Route exact path="/team" component={ Team }/>
                   <Route exact path="/room" component={ Room }/>
-                  <Route exact path="/cancelroom" component={ Room }/>
+                  <Route exact path="/cancelroom" component={ CancelRoom }/>
                   <Route exact path="/project" component={ Project }/>
             </Switch>
 		</>
